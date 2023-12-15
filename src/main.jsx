bc4969c327f63e0d9c60e235fcc23402d419aaa1
Eme Hache <luisalfonsodsg@outlook.com>
2023-12-15 15:07:09 -0500

    refactor: reorder folder structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 import React from 'react'
-import App from './App'
+import App from './pages/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/pages/App.jsx
@@ -0,0 +1,14 @@
+import { WeatherProvider } from '../context/WeatherProvider'
+import Header from '../components/Header'
+import Main from '../components/Main'
+
+const App = () => {
+  return (
+    <WeatherProvider>
+      <Header />
+      <Main />
+    </WeatherProvider>
+  )
+}
+
+export default App
